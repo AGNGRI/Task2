bc8c6c4a35db8c0c7a1dfe285df45d231e4a0a4e
AGNGRI <agne.jacunskaite@gmail.com>
2022-12-18 16:56:13 +0200

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            var number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            var number2 = int.Parse(Console.ReadLine());
+            int sum = number1 + number2;
+            Console.WriteLine($"Calculated sum is: {sum}");
+        }
+    }
+}
